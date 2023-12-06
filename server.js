93aa9eca6222deeaa2b4f03b19cd39469af20e9b
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2023-12-05 18:22:22 -0600

    added save note button function to server.js

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
-// Create all the needed dependencies and routes
 const express = require('express');
-const apiRoute = require('./routes/api-route');
-const htmlRoute = require('./routes/html-route');
-const PORT = process.env.PORT || 3001;
+const path = require('path');
 const app = express();
-// Sets up the middleware to parse json data
-app.use(express.urlencoded({ extended: true }));
+const PORT = 3001;
+const fs = require('fs');
+
 app.use(express.json());
-
 app.use(express.static('public'));
-// Sets up the routes to the routes folder
-app.use(apiRoute);
-app.use(htmlRoute);
 
-app.listen(PORT);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text ) {
+    const newNote = {
+      title,
+      text
+    };
+    
+    const response = {
+      body: newNote,
+    };
+
+    res.status(201).json(response);
+    fs.appendFile(__dirname + '/db/db.json', JSON.stringify(response, null, 2) + '\n', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log('Data has been written to the file');
+      }
+    });
+  } else {
+    res.status(500).json('Error in saving note');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
